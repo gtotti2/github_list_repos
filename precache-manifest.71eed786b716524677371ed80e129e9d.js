26a34a76cda0a99d644abdaf1c970efc7cdb7f89
Gian Lucca Ricupito Dos Santos <gian.santos@saraiva.com.br>
2019-03-01 16:46:36 -0300

    Updates

---
+++ precache-manifest.71eed786b716524677371ed80e129e9d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/github_list_repos/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "04ecb0d680c862ab7e81",
+    "url": "/github_list_repos/static/css/main.36f28251.chunk.css"
+  },
+  {
+    "revision": "4ab2a53a418bd2b113ec",
+    "url": "/github_list_repos/static/js/runtime~main.4ab2a53a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/github_list_repos/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a3bd21ceab6f9c51596a",
+    "url": "/github_list_repos/static/js/2.a3bd21ce.chunk.js"
+  },
+  {
+    "revision": "04ecb0d680c862ab7e81",
+    "url": "/github_list_repos/static/js/main.04ecb0d6.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/github_list_repos/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/github_list_repos/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/github_list_repos/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a3bd21ceab6f9c51596a",
+    "url": "/github_list_repos/static/css/2.7fe36530.chunk.css"
+  },
+  {
+    "revision": "50c6668c15acb34e1b5beb369644b088",
+    "url": "/github_list_repos/index.html"
+  }
+];
