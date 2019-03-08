ca3dc0745e9e81c35d28d0f3fa6e59215453f8e1
Gian Lucca Ricupito Dos Santos <gian.santos@saraiva.com.br>
2019-03-08 11:27:01 -0300

    Updates

--- precache-manifest.e35b207aff935d6fa89e9a3373b7548a.js
+++ precache-manifest.e35b207aff935d6fa89e9a3373b7548a.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/github_list_repos/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "15b7ee3a726b4785487e",
+    "revision": "d3492218a6f2d8dcb80f",
     "url": "/github_list_repos/static/css/main.36f28251.chunk.css"
   },
   {
@@ -16,31 +16,31 @@ self.__precacheManifest = [
     "url": "/github_list_repos/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "a3bd21ceab6f9c51596a",
-    "url": "/github_list_repos/static/js/2.a3bd21ce.chunk.js"
+    "revision": "77fbf9244436b68bb95f",
+    "url": "/github_list_repos/static/js/2.77fbf924.chunk.js"
   },
   {
-    "revision": "15b7ee3a726b4785487e",
-    "url": "/github_list_repos/static/js/main.15b7ee3a.chunk.js"
+    "revision": "d3492218a6f2d8dcb80f",
+    "url": "/github_list_repos/static/js/main.d3492218.chunk.js"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/github_list_repos/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/github_list_repos/static/media/fontawesome-webfont.b06871f2.ttf"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/github_list_repos/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "a3bd21ceab6f9c51596a",
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/github_list_repos/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "77fbf9244436b68bb95f",
     "url": "/github_list_repos/static/css/2.7fe36530.chunk.css"
   },
   {
-    "revision": "3f4de893a29ab221934d6800a83999e0",
+    "revision": "a7a86fdc7b847ac8ed8184cecec06dcc",
     "url": "/github_list_repos/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github_list_repos/precache-manifest.4858fa3b192705ae5e51ea5fa40c8983.js"
+  "/github_list_repos/precache-manifest.e35b207aff935d6fa89e9a3373b7548a.js"
 );
 
 workbox.clientsClaim();
